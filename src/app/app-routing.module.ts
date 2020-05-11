e602868c57ea84c912ae8737a627f9d3fea87c93
sebanob9 <52758114+sebanob9@users.noreply.github.com>
2020-05-11 18:28:52 +0200

    Created project Included main components + routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PricesComponent } from './components/prices/prices.component';
+import { RestrictedComponent } from './components/restricted/restricted.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'prices', component: PricesComponent},
+  {path: 'restricted', component: RestrictedComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'home'},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/restricted/restricted.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-restricted',
+  templateUrl: './restricted.component.html',
+  styles: []
+})
+export class RestrictedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
